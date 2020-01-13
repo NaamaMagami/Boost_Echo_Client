f539fda531c44d63160963c72ce515aa4c6fa11b
magami@wincs.cs.bgu.ac.il <naamamagami2@gmail.com>
2020-01-13 17:28:51 +0200

    17:28

---
+++ include/readFromServ.h
@@ -0,0 +1,21 @@
+//
+// Created by magami@wincs.cs.bgu.ac.il on 13/01/2020.
+//
+
+#ifndef BOOST_ECHO_CLIENT_READFROMSERV_H
+#define BOOST_ECHO_CLIENT_READFROMSERV_H
+
+
+#include "connectionHandler.h"
+
+class readFromServ {
+public:
+    readFromServ(ConnectionHandler &);
+private:
+    ConnectionHandler &handler ;
+
+
+};
+
+
+#endif //BOOST_ECHO_CLIENT_READFROMSERV_H
---
+++ src/readFromServ.cpp
@@ -0,0 +1,19 @@
+    //
+    // Created by magami@wincs.cs.bgu.ac.il on 13/01/2020.
+    //
+
+    #include "readFromServ.h"
+    #include "connectionHandler.h"
+    using namespace std;
+
+    readFromServ::readFromServ(ConnectionHandler& _handler1):handler(_handler1){}
+    handler getline
+    if(=="MESSAGE"){
+
+        }
+    if(=="RECEIPT"){
+
+    }
+    if(=="ERROR"){
+
+    }
